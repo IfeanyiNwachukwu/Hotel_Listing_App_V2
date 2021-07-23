b4d08f08c55eacf28b943f9e8a1482fe8d6d3f38
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-07-23 14:17:09 +0100

    Added the Repository and Unit of Work SetUp

---
+++ HotelListingAppVersion2/IRespository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using HotelListingAppVersion2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListingAppVersion2.IRespository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+
+        Task Save();
+    }
+}
---
+++ HotelListingAppVersion2/Repository/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using HotelListingAppVersion2.Data;
+using HotelListingAppVersion2.IRespository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListingAppVersion2.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries { get; }
+        private IGenericRepository<Hotel> _hotels { get; }
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+
+        }
+        public IGenericRepository<Country> Countries => _countries?? new GenericRepository<Country>(_context);
+
+        public IGenericRepository<Hotel> Hotels => _hotels?? new GenericRepository<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
