53fd225e419f2d3295fb4b0f0274d0c7f8bcc04e
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-07-24 12:41:12 +0100

    Constructed the Getall EndPoint

--- HotelListingAppVersion2/Startup.cs
+++ HotelListingAppVersion2/Startup.cs
@@ -1,5 +1,7 @@
 using HotelListingAppVersion2.Configurations;
 using HotelListingAppVersion2.Data;
+using HotelListingAppVersion2.IRespository;
+using HotelListingAppVersion2.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +35,7 @@ namespace HotelListingAppVersion2
                 });
 
             services.AddAutoMapper(typeof(MapperInitializer));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 
             // Fr Adding Swagger
@@ -41,7 +44,7 @@ namespace HotelListingAppVersion2
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListingAppVersion2", Version = "v1" });
             });
             
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
